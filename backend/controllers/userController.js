9ec9df9eaac7f37f93a12e488ce0bf61bebe9625
NadeesH <spnadeeshnimsara@gmail.com>
2024-01-10 13:56:34 +0530

    Create login route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,10 +16,26 @@ const signupUser = async (req, res) => {
     // assign a token
     const token = createToken(user._id);
 
-    res.status(200).json({ user, token });
+    res.status(200).json({ user, token, msg: 'Registered' });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-module.exports = { signupUser };
+// login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // assign a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token, msg: 'Logged in' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { signupUser, loginUser };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 // controller functions
-const { signupUser } = require('../controllers/userController');
+const { signupUser, loginUser } = require('../controllers/userController');
 
 const router = express.Router();
 
 // signup route
 router.post('/signup', signupUser);
+
+router.post('/login', loginUser);
 
 module.exports = router;
