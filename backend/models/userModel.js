7d1428cdbcf616874d0c6e67d357e4a0895c4368
NadeesH <spnadeeshnimsara@gmail.com>
2024-01-10 11:30:48 +0530

    Create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstname: {
+    type: String,
+    require: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
