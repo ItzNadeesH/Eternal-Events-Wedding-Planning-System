8233a910081ce5424e746ffb973e8b4e9445e90b
NadeesH <spnadeeshnimsara@gmail.com>
2024-01-10 16:02:05 +0530

    Create Signup Form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Components
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
